79fe49869d787e3883498179a618f49e20bc0c2f
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-10-30 23:42:13 +0100

    scrape scrape

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -2,24 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-	response = requests.get(
-		url="https://en.wikipedia.org/wiki/List_of_fictional_countries"
-	)
-	soup = BeautifulSoup(response.content, 'html.parser')
+	f = open("baby_names_boys.txt", "w+")
+	for i in range(2, 11):
+		response = requests.get(
+			url=f"https://www.vernoeming.nl/alle-jongensnamen-van-{i}-letters/"
+		)
+		soup = BeautifulSoup(response.content, 'html.parser')
 
-	# Get all the links
-	allLinks = soup.find(id="bodyContent").find_all("td")
+		# Get all the links
+		allLinks = soup.find_all("li")
 
 
-	names = []
+		names = []
 
-	f = open("fictional_nation.txt", "w")
-	for link in allLinks:
-		t = link.text.strip()
-		if len(t) < 20:
-			names.append(t)
-	for i, name in enumerate(names):
-		if i + 1 < len(names):
-			if len(names[i+1]) == 0:
-				f.write(name + '\n')
+		for link in allLinks:
+			t = link.text.strip()
+			f.write(t + '\n')
 	f.close()
